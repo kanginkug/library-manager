204f41f014013bea12ef814a66611af0f4960ac9
kik <rkddlsrnr1234@gmail.com>
2023-09-05 12:23:23 +0900

    first

---
+++ src/test/java/com/testpractice/testpractice/TestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testpractice.testpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
