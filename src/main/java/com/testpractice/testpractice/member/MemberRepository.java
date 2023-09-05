c75046173932ca814d285361410fe0f623774c16
kik <rkddlsrnr1234@gmail.com>
2023-09-05 20:41:34 +0900

    sign up create

--- src/main/java/com/testpractice/testpractice/member/MemberRepository.java
+++ src/main/java/com/testpractice/testpractice/member/MemberRepository.java
@@ -3,5 +3,13 @@ package com.testpractice.testpractice.member;
 import com.testpractice.testpractice.rental.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsById(String memberId);
+
+    List<Member> findAllById(String memberId);
+
+    boolean existsByPhone(String phone);
 }
